9b0f2d34483c0f2e28f4e1f0e0c0c0b09302fb71
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-12-03 11:27:14 +0100

    main.py :: main(): removed temporary comments

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from a_star import AStar
 
 def main():
     """start the assignment"""
-    # task1 = AStar(task=1)
-    # task1.compute()
-    # task1.print()
+    task1 = AStar(task=1)
+    task1.compute()
+    task1.print()
 
-    # task2 = AStar(task=2, distance="manhattan")
-    # task2.compute()
-    # task2.print()
+    task2 = AStar(task=2, distance="manhattan")
+    task2.compute()
+    task2.print()
 
-    task3 = AStar(task=3, distance="manhattan2")
+    task3 = AStar(task=3)
     task3.compute()
     task3.print()
 
-    # task3 = AStar(task=4)
-    # task3.compute()
-    # task3.print()
+    task3 = AStar(task=4)
+    task3.compute()
+    task3.print()
 
 
 if __name__ == "__main__":
